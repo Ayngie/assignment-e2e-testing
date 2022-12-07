1459d3ebbcbd3d3cf52458d9cf599026ca015640
Angelica Reuterswärd <angelica@macbook-pro.lan>
2022-12-07 23:56:47 +0100

    Tagit bort oanvänd testdata i min mocksfil

--- cypress/mocks/movieMocks.ts
+++ cypress/mocks/movieMocks.ts
@@ -21,16 +21,3 @@ export const mockData: IOmdbResponse = {
     },
   ],
 };
-
-export const halfOfMockData: IOmdbResponse = {
-  Search: [
-    {
-      Title: "Star Wars: Episode IV - A New Hope (Mock)",
-      imdbID: "tt0076759",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-      Year: "1977",
-    },
-  ],
-};
